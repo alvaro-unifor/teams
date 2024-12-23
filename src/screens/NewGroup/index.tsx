dd22824eb9f891df9776fc9022bf5369d0a56b26
Alvaro Bezerra <alvaro.a.bezerra@hmit.com.br>
2024-12-23 15:49:38 -0300

    Nova interface

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,33 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+
+
+import { Container, Content, Icon } from "./styles";
+import { Button } from "@components/Button";
+
+export function NewGroup() {
+  return (
+    <Container>
+        <Header showBackButton/>
+
+        <Content>
+            <Icon />
+
+            <Highlight 
+                title="Nova turma"
+                subtitle="crie uma turma para adicionar pessoas"
+            />
+
+            <Input 
+              placeholder="Nome da turma"
+            />
+
+            <Button 
+                title="Criar"
+                style={{ marginTop: 20 }}
+            />
+        </Content>
+    </Container>
+  );
+}
